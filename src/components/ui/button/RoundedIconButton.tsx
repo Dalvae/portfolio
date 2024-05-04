a7a299465e40adaddd9f467137003f5c9a0c8b14
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-05-03 20:43:21 -0400

    Component withrefs

--- src/components/ui/button/RoundedIconButton.tsx
+++ src/components/ui/button/RoundedIconButton.tsx
@@ -1,24 +1,27 @@
 import { forwardRef } from "react";
-
 import { clsxm } from "@/lib/helper";
-
 import { MotionButtonBase } from "./MotionButton";
+import type { ComponentPropsWithRef } from "react";
+import type { MotionProps } from "framer-motion";
 
-export const RoundedIconButton: typeof MotionButtonBase = forwardRef(
-  ({ className, children, ...rest }, ref) => {
-    return (
-      <MotionButtonBase
-        ref={ref}
-        className={clsxm(
-          "inline-flex rounded-full bg-accent p-2 text-center leading-none center hover:opacity-90",
-          className
-        )}
-        {...rest}
-      >
-        {children}
-      </MotionButtonBase>
-    );
-  }
-);
+type RoundedIconButtonProps = ComponentPropsWithRef<"button"> & MotionProps;
+
+export const RoundedIconButton = forwardRef<
+  HTMLButtonElement,
+  RoundedIconButtonProps
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <MotionButtonBase
+      ref={ref}
+      className={clsxm(
+        "inline-flex rounded-full bg-accent p-2 text-center leading-none center hover:opacity-90",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </MotionButtonBase>
+  );
+});
 
 RoundedIconButton.displayName = "RoundedIconButton";
