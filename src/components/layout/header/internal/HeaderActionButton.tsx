62e496704a41f3d3d4576c6bfca89c1ac7af81d5
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-05-03 20:26:38 -0400

    Fixed forwardrefRender of HeaderActionButton

--- src/components/layout/header/internal/HeaderActionButton.tsx
+++ src/components/layout/header/internal/HeaderActionButton.tsx
@@ -1,18 +1,20 @@
-import { forwardRef } from 'react'
-import clsx from 'clsx'
-import type { ForwardRefComponent } from 'framer-motion'
+import { forwardRef } from "react";
+import clsx from "clsx";
+import type { ForwardRefRenderFunction } from "react";
 
-export const HeaderActionButton: ForwardRefComponent<
+export const HeaderActionButton: ForwardRefRenderFunction<
   HTMLButtonElement,
-  JSX.IntrinsicElements['button']
-> = forwardRef(({ children, ...rest }, ref) => {
+  JSX.IntrinsicElements["button"]
+> = (props, ref) => {
+  const { children, ...rest } = props;
+
   return (
     <button
       className={clsx(
-        'group h-10 w-10 rounded-full bg-gradient-to-b',
-        'from-zinc-50/50 to-white/90 px-3 text-sm ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20',
-        'shadow-lg shadow-zinc-800/5',
-        'flex center',
+        "group h-10 w-10 rounded-full bg-gradient-to-b",
+        "from-zinc-50/50 to-white/90 px-3 text-sm ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20",
+        "shadow-lg shadow-zinc-800/5",
+        "flex center"
       )}
       {...rest}
       ref={ref}
@@ -20,7 +22,9 @@ export const HeaderActionButton: ForwardRefComponent<
     >
       {children}
     </button>
-  )
-})
+  );
+};
 
-HeaderActionButton.displayName = 'HeaderActionButton'
+export default forwardRef(HeaderActionButton);
+
+HeaderActionButton.displayName = "HeaderActionButton";
