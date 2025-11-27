497ac02565a34a15b4d95fe8ec8ce05ee7543573
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-11-27 14:28:33 -0300

    Remove old favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -123,7 +123,6 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
-        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
         { rel: "canonical", href: "https://diegoalvarez.dev" },
       ],
     },
